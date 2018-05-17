f7ad5e9010f9f932adb7955684d5b88715306ac8
Admin <admin@mac-admin.local>
2018-05-17 15:07:17 +0700

    Initial Commit

---
+++ bnrg_01_quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bnrg_01_quiz
+//
+//  Created by Admin on 17.05.2018.
+//  Copyright © 2018 Roman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
