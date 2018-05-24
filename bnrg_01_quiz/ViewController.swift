cd62f8cbaa4dc89412a596b2e0cb3dfd62f2dd1f
Roman <cold.steel.br@gmail.com>
2018-05-24 15:20:22 +0700

    Animations in-out added

--- bnrg_01_quiz/ViewController.swift
+++ bnrg_01_quiz/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentQuestionLabel: UILabel!
     @IBOutlet weak var nextQuestionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet weak var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
     
     let questions: [String] = [
         "Capital of Russia?",
@@ -31,6 +33,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         currentQuestionLabel.text = questions[currentQuestionIndex]
+        updateOffScreenLabel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -57,19 +60,34 @@ class ViewController: UIViewController {
         answerLabel.text = answer
     }
     
+    func updateOffScreenLabel(){
+        let screenWidth = view.frame.width
+        nextQuestionLabelCenterXConstraint.constant = -screenWidth
+    }
     
     func animateLabelTransitions(){
+        view.layoutIfNeeded()
         // Animation
+        let screenWidth = view.frame.width
+        self.nextQuestionLabelCenterXConstraint.constant = 0
+        self.currentQuestionLabelCenterXConstraint.constant += screenWidth
+        
         UIView.animate(withDuration: 0.5,
                        delay: 0,
-                       options: [],
+                       options: [.curveEaseInOut],
+                       //options: [],
                        animations: {
                             () -> Void in
                             self.currentQuestionLabel.alpha = 0
                             self.nextQuestionLabel.alpha = 1
+                        
+                            self.view.layoutIfNeeded()
                         },
                        completion: { _ in
                             swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+                            swap(&self.currentQuestionLabelCenterXConstraint, &self.nextQuestionLabelCenterXConstraint)
+                        
+                            self.updateOffScreenLabel()
                         }
                        )// animate
     } // func
